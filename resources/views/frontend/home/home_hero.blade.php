977e02902df5f1dc67d572c1689b2f7ca13bf32b
Mohamed Biuk <moebiuk98@gmail.com>
2024-02-09 17:37:18 +0200

    Homepage and some configes

---
+++ resources/views/frontend/home/home_hero.blade.php
@@ -0,0 +1,22 @@
+<section class="bg-transparent dark:bg-gray-900" style="margin-top: 5%">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Discover and Explore Our Country Like Never Before</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Embark on an Unforgettable Journey Through Our Nation's Untamed Wonders.</p>
+            <div class="flex mt-16">
+                <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Get started
+                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+                <a href="#" class="inline-flex items-center rounded-lg justify-center px-5 py-3 text-base font-medium text-center text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    Download app <span class="pl-2"><svg class="w-6 h-6 text-teal-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"/>
+                    </svg></span>
+                </a> 
+            </div>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex" >
+            <img src="{{ asset ('frontend/assets/Hero image.png')}}" style="position: absolute; right: 0; top:0; width: 550px; margin-top: 5%" alt="mockup">
+        </div>                
+    </div>
+</section>
---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,16 @@
+@extends('frontend.master_dashboard')
+
+
+@section('main')
+
+
+@section('title')
+    OasisQuest - Explore the Nation
+@endsection
+
+
+@include('frontend.home.home_hero')
+
+
+
+@endsection
