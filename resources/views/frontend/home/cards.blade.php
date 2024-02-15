4c64436836a25bb5f3ac27672a299798bac7cb7c
Moezz-GH <you@example.com>
2024-02-15 02:56:59 +0200

    about us

--- resources/views/frontend/home/cards.blade.php
+++ resources/views/frontend/home/cards.blade.php
@@ -1,7 +1,7 @@
 <section class="bg-transparent dark:bg-gray-900" style="margin-top:100px">
     <div class="py-8 px-12 mx-auto max-w-screen-lg sm:py-1 lg:px-6">
         <div class="text-center mb-8 lg:mb-16">
-            <span class="bg-teal-800 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-teal-900 dark:text-blue-300">Experiences</span>
+            <span class="bg-teal-800 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-teal-900 dark:text-blue-300">Categories</span>
             <h2 class="mb-4 mt-2 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Available Activites</h2>
             <p class=" text-gray-500 sm:text-xl dark:text-gray-400">There are many variations of passages of Lorem Ipsum available
                 but the majority have suffered alteration in some form.</p>
@@ -13,7 +13,7 @@
                 background: linear-gradient(180deg, rgba(0,0,0,0.2497373949579832) 0%, rgba(10,9,9,0.8) 100%);">
                     <div class="absolute inset-x-0 bottom-0 p-5">
                         <a href="#">
-                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Camping</h5>
+                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Outdoor</h5>
                         </a>
                         <a href="#" class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300 z-10 inline-flex items-center justify-center px-5 py-3 mr-3 text-xs font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             View Details
@@ -22,12 +22,12 @@
                     </div>
                 </div>      
             </div>
-            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1559521783-1d1599583485?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
+            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1517649763962-0c623066013b?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
                 <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm rounded-lg "style="background: rgb(0,0,0);
                 background: linear-gradient(180deg, rgba(0,0,0,0.2497373949579832) 0%, rgba(10,9,9,0.8) 100%); ">
                     <div class="absolute inset-x-0 bottom-0 p-5">
                         <a href="#">
-                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Camping</h5>
+                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Sport</h5>
                         </a>
                         <a href="#" class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300 z-10 inline-flex items-center justify-center px-5 py-3 mr-3 text-xs font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             View Details
@@ -36,12 +36,12 @@
                     </div>
                 </div>      
             </div>
-            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1559521783-1d1599583485?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
+            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1580982327559-c1202864eb05?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
                 <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm rounded-lg "style="background: rgb(0,0,0);
                 background: linear-gradient(180deg, rgba(0,0,0,0.2497373949579832) 0%, rgba(10,9,9,0.8) 100%); ">
                     <div class="absolute inset-x-0 bottom-0 p-5">
                         <a href="#">
-                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Camping</h5>
+                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Agriculture</h5>
                         </a>
                         <a href="#" class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300 z-10 inline-flex items-center justify-center px-5 py-3 mr-3 text-xs font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             View Details
@@ -50,12 +50,12 @@
                     </div>
                 </div>      
             </div>
-            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1559521783-1d1599583485?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
+            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1621784166258-c6fdfff31879?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
                 <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm rounded-lg "style="background: rgb(0,0,0);
                 background: linear-gradient(180deg, rgba(0,0,0,0.2497373949579832) 0%, rgba(10,9,9,0.8) 100%); ">
                     <div class="absolute inset-x-0 bottom-0 p-5">
                         <a href="#">
-                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Camping</h5>
+                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Art</h5>
                         </a>
                         <a href="#" class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300 z-10 inline-flex items-center justify-center px-5 py-3 mr-3 text-xs font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             View Details
@@ -64,12 +64,12 @@
                     </div>
                 </div>      
             </div>
-            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1559521783-1d1599583485?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
+            <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"style="background-image: url('https://images.unsplash.com/photo-1538511059256-46e76f13f071?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); ">
                 <div class="h-80 bg-cover relative bg-center bg-no-repeat text-center max-w-sm rounded-lg "style="background: rgb(0,0,0);
                 background: linear-gradient(180deg, rgba(0,0,0,0.2497373949579832) 0%, rgba(10,9,9,0.8) 100%); ">
                     <div class="absolute inset-x-0 bottom-0 p-5">
                         <a href="#">
-                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Camping</h5>
+                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white">Indoor</h5>
                         </a>
                         <a href="#" class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300 z-10 inline-flex items-center justify-center px-5 py-3 mr-3 text-xs font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             View Details
